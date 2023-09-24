69670d1e7bf92ae1b04ca8916750117584fbbdff
BLUETECHIE05 <141574099+BLUETECHIE05@users.noreply.github.com>
2023-09-25 01:22:13 +0530

    Update #1.py
    CORRECTED QUES 1

--- lab4.py/#1.py
+++ lab4.py/#1.py
@@ -1,10 +1,8 @@
 N=int(input("ENTER A NUMBER "))
 i=1
 while i<21:
-    if N<0:
-     print(N,'*',i,'=',N*i)
-     i+=1
+    if N>0:
+        print(N,'*',i,'=',N*i)
+        i+=1
     else:
-        print("enter a positive number")
-
-    
+        print("NOT VALID ")
